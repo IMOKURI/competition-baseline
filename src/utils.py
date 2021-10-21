0c5a94a55f86d7ae9cfdceb199226df1512a7ed5
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-10-21 15:33:37 +0900

    update

--- src/utils.py
+++ src/utils.py
@@ -19,6 +19,8 @@ log = logging.getLogger("__main__").getChild("utils")
 
 
 def gpu_settings(c):
+    if os.environ["CUDA_VISIBLE_DEVICES"] is not None:
+        return
     try:
         os.environ["CUDA_VISIBLE_DEVICES"] = c.settings.gpus
     except ConfigAttributeError:
